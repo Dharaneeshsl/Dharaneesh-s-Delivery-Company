878068ce99e63d5919ac6742b39dfb7d0cfbc26c
Dharaneeshsl <dharaneesh19112006@gmail.com>
2025-07-14 02:13:56 +0530

    Fix Flutter navigation by registering named routes for /home and /login

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:dharaneesh_delivery/providers/auth_provider.dart';
 import 'package:dharaneesh_delivery/providers/delivery_provider.dart';
 import 'package:dharaneesh_delivery/screens/splash_screen.dart';
 import 'package:dharaneesh_delivery/utils/theme.dart';
+import 'package:dharaneesh_delivery/screens/home_screen.dart';
+import 'package:dharaneesh_delivery/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,6 +28,10 @@ class MyApp extends StatelessWidget {
         darkTheme: AppTheme.darkTheme,
         themeMode: ThemeMode.system,
         home: const SplashScreen(),
+        routes: {
+          '/home': (context) => HomeScreen(),
+          '/login': (context) => LoginScreen(),
+        },
       ),
     );
   }
